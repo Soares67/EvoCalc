5def41f9b668d1aa26f00cd916b4bceb12375871
Adriel Soares <adriellsoares067@gmail.com>
2024-01-23 21:14:44 -0400

    .

--- testes.py
+++ testes.py
@@ -3,3 +3,18 @@ y = "100*5%"
 z = "41/100*1929"
 print(eval(z))
 
+
+obs = """
+Paleta de cores:
+#2ec68b   (Shamrock)
+#4fc7ee   (Picton Blue)
+#a3b0f6   (Perano)
+#242424   (Mine Shaft)
+
+------------------------
+
+*O limite de caracteres no output foi definido como 13
+    *Caso a quantidade de caracteres ultrapasse o limite não é possível inserir mais caracteres
+    *Mesmo se o último caractere digitado for um operador e o limite tiver sido alcançado não será possível inserir caracteres
+    *Caso exceda o limite de caracteres basta apagar
+"""
