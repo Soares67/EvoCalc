28071b120da17d33daee09f292825e1c0428f8c9
Adriel Soares <adriellsoares067@gmail.com>
2024-04-09 16:13:03 -0400

    Update Calc.py
    Bug fixes

--- Calc.py
+++ Calc.py
@@ -8,7 +8,6 @@ class Calculadora:
         self.erro = "Erro"
         self._entry = entry
 
-
     def change_color(self,  root, lb_letreiro):
         r = random.randint(0,255)
         g = random.randint(0,255)
@@ -16,14 +15,13 @@ class Calculadora:
         color = f"#{r:02x}{g:02x}{b:02x}"
         lb_letreiro.configure(text_color=color)
         root.after(600, self.change_color, lb_letreiro, root)
-
+    
     # 0
     def bt0(self):
         if self.divzero in self._entry.get() or self.erro in self._entry.get():
             self._entry.delete(0, "end")
         if len(self._entry.get()) < self._limite:
             self._entry.insert(0+len(self._entry.get()), "0")
-
 
     # 1
     def bt1(self):
@@ -32,14 +30,12 @@ class Calculadora:
         if len(self._entry.get()) < self._limite:
             self._entry.insert(0+len(self._entry.get()), "1")
 
-
     # 2
     def bt2(self):
         if self.divzero in self._entry.get() or self.erro in self._entry.get():
             self._entry.delete(0, "end")
         if len(self._entry.get()) < self._limite:
             self._entry.insert(0+len(self._entry.get()), "2")
-
 
     # 3
     def bt3(self):
@@ -48,14 +44,12 @@ class Calculadora:
         if len(self._entry.get()) < self._limite:
             self._entry.insert(0+len(self._entry.get()), "3")
 
-
     # 4
     def bt4(self):
         if self.divzero in self._entry.get() or self.erro in self._entry.get():
             self._entry.delete(0, "end")
         if len(self._entry.get()) < self._limite:
             self._entry.insert(0+len(self._entry.get()), "4")
-
 
     #  5
     def bt5(self):
@@ -64,14 +58,12 @@ class Calculadora:
         if len(self._entry.get()) < self._limite:
             self._entry.insert(0+len(self._entry.get()), "5")
 
-
     # 6
     def bt6(self):
         if self.divzero in self._entry.get() or self.erro in self._entry.get():
             self._entry.delete(0, "end")
         if len(self._entry.get()) < self._limite:
             self._entry.insert(0+len(self._entry.get()), "6")
-
 
     # 7
     def bt7(self):
@@ -80,14 +72,12 @@ class Calculadora:
         if len(self._entry.get()) < self._limite:
             self._entry.insert(0+len(self._entry.get()), "7")
 
-
     # 8
     def bt8(self):
         if self.divzero in self._entry.get() or self.erro in self._entry.get():
             self._entry.delete(0, "end")
         if len(self._entry.get()) < self._limite:
             self._entry.insert(0+len(self._entry.get()), "8")
-
 
     # 9
     def bt9(self):
@@ -96,14 +86,13 @@ class Calculadora:
         if len(self._entry.get()) < self._limite:
             self._entry.insert(0+len(self._entry.get()), "9")
 
-
     # Vírgula
     def btcomma(self):
         if self.divzero in self._entry.get() or self.erro in self._entry.get():
             self._entry.delete(0, "end")
         if len(self._entry.get()) < self._limite and len(self._entry.get()) > 0:
-            self._entry.insert(0+len(self._entry.get()), ".")
-
+            if not "." in self._entry.get():
+                self._entry.insert(0+len(self._entry.get()), ".")
 
     # Funções das operações
 
@@ -114,14 +103,12 @@ class Calculadora:
         if len(self._entry.get()) > 0:
             self._entry.delete(0, "end")
 
-
     # Apagar um dígito (<X)
     def clearone(self):
         if self.divzero in self._entry.get() or self.erro in self._entry.get():
             self._entry.delete(0, "end")
         if len(self._entry.get()) > 0:
             self._entry.delete(int(len(self._entry.get().replace(",", "").replace(".",""))-1))
-
 
     # Soma
     def btsum(self):
@@ -131,7 +118,6 @@ class Calculadora:
             if not "+" in self._entry.get():
                 self._entry.insert(0+len(self._entry.get()), "+")
 
-
     # Multiplicação
     def btmult(self):
         if self.divzero in self._entry.get() or self.erro in self._entry.get():
@@ -139,7 +125,6 @@ class Calculadora:
         if len(self._entry.get()) < self._limite and len(self._entry.get()) > 0:
             if not "*" in self._entry.get():
                 self._entry.insert(0+len(self._entry.get()), "*")
-
 
     # Subtração
     def btmin(self):
@@ -149,7 +134,6 @@ class Calculadora:
             if not "-" in self._entry.get():
                 self._entry.insert(0+len(self._entry.get()), "-")
 
-
     # Divisão
     def btdiv(self):
         if self.divzero in self._entry.get() or self.erro in self._entry.get():
@@ -158,15 +142,12 @@ class Calculadora:
             if not "/" in self._entry.get():
                 self._entry.insert(0+len(self._entry.get()), "/")
 
-
     # Porcentagem
     def btpercent(self):
         if self.divzero in self._entry.get() or self.erro in self._entry.get():
             self._entry.delete(0, "end")
         if len(self._entry.get()) < self._limite and len(self._entry.get()) > 0:
-            if not "%" in self._entry.get():
                 self._entry.insert(0+len(self._entry.get()), "%")
-
 
     # Resultado
     def btres(self):
